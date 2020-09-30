85b8865f993e32f6291fdfbc1d00b1d8c45f8789
guilefoylegaurav <grvb1515@gmail.com>
2020-09-30 10:34:36 +0530

    Added feed

---
+++ lib/screens/feed.dart
@@ -0,0 +1,68 @@
+import 'package:dsc_club_management_app/services/data.dart';
+import 'package:dsc_club_management_app/widgets/banner.dart';
+import 'package:flutter/material.dart';
+
+class Feed extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Feed"),
+          elevation: 0,
+          centerTitle: true,
+        ),
+        drawer: Drawer(
+          child: Center(
+            child: Text("Under Construction"),
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              BannerS(),
+              Container(
+                child: Column(
+                  children: EventList.map((e) {
+                    return InkWell(
+                      onTap: () {
+                        Navigator.of(context).pushNamed("/event");
+                      },
+                      child: ListTile(
+                        leading: CircleAvatar(
+                          child: Container(
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                image: DecorationImage(
+                                    image: AssetImage("assets/code.png"))),
+                          ),
+                        ),
+                        title: Text(e.name),
+                        subtitle: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Organised by ${e.club}"),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text("${e.date} | ${e.time}"),
+                                Text(
+                                  "ENDED",
+                                  style: TextStyle(color: Colors.red),
+                                )
+                              ],
+                            ),
+                            SizedBox(
+                              height: 5,
+                            )
+                          ],
+                        ),
+                      ),
+                    );
+                  }).toList(),
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
+}
