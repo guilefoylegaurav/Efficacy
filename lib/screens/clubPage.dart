566dab55415e77919b690d4954419370f3a917f4
guilefoylegaurav <grvb1515@gmail.com>
2020-11-04 15:59:28 +0530

    Making page for the description of a single club dynamic in accordance to DRY principles. #3

--- lib/screens/clubPage.dart
+++ lib/screens/clubPage.dart
@@ -7,6 +7,11 @@ import '../services/data.dart';
 class ClubPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final id = routeArgs['id'];
+    var club = clubList.firstWhere((element) => element.id == id);
+    var events = eventList.where((element) => element.club == club.name);
     return Scaffold(
       backgroundColor: Color(hexColor(BG)),
       body: CustomScrollView(
@@ -15,23 +20,49 @@ class ClubPage extends StatelessWidget {
             floating: false,
             pinned: true,
             flexibleSpace: FlexibleSpaceBar(
-              centerTitle: true,
-              title: Text(
-                "Illuminits",
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              background: Container(
-                color: Color(hexColor(BG)),
-              ),
-            ),
+                centerTitle: true,
+                title: Text(
+                  club.name,
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                background: Stack(
+                  children: [
+                    Container(
+                      child: Image.network(
+                        "https://lh3.googleusercontent.com/proxy/dvv3PVmRr-U82XIbhzrfJt57vBgAtE7uk1y8wye3dbhWgt-l9lnJjyKDR33uvdjRMi6wPnhqP9N4QpXcLLyfkkLKn65TBuPOwfI9AUGEGK6EW24kvNyO54Aj1xzpgTijSfLy9pr0Ip-5g-NA1P3Nrprz0qzSvT2iYqXV",
+                        fit: BoxFit.cover,
+                        width: double.infinity,
+                      ),
+                    ),
+                    Positioned(
+                      child: Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(30),
+                                topRight: Radius.circular(30)),
+                            color: Colors.white),
+                        height: 600,
+                      ),
+                    )
+                  ],
+                )),
             expandedHeight: 200,
           ),
           SliverList(
               delegate: SliverChildListDelegate([
             Container(
-              padding: EdgeInsets.fromLTRB(10, 40, 10, 5),
-              child: Column(
-                children: EventList.map((e) => EventTile(e: e)).toList(),
+              padding: EdgeInsets.fromLTRB(10, 5, 10, 5),
+              child: SingleChildScrollView(
+                child: Column(children: [
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    child: Column(
+                      children: events.map((e) => EventTile(e: e)).toList(),
+                    ),
+                  )
+                ]),
               ),
               decoration: BoxDecoration(
                   borderRadius: BorderRadius.only(
