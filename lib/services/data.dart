b34ef637038ce3ac686db8bf7dae91ac0c41ef6b
guilefoylegaurav <grvb1515@gmail.com>
2020-11-04 16:20:35 +0530

    Adding club data to be rendered in the club page. #3

--- lib/services/data.dart
+++ lib/services/data.dart
@@ -1,6 +1,7 @@
+import 'package:dsc_club_management_app/models/club.dart';
 import 'package:dsc_club_management_app/models/event.dart';
 
-var EventList = [
+var eventList = [
   Event(name: "Cloud Essentials", club: "DSC", date: "27 Sep", time: "19 : 00"),
   Event(
       name: "Overfitting and Underfitting Class",
@@ -8,5 +9,50 @@ var EventList = [
       date: "27 Sep",
       time: "8 : 00"),
   Event(name: "Parlare", club: "Illuminits", date: "27 Sep", time: "9 : 00"),
+  Event(
+      name: "DSA Class", club: "Coding Club", date: "27 Sep", time: "11 : 00"),
+  Event(
+      name: "Intro to Robotics",
+      club: "NERDS",
+      date: "27 Sep",
+      time: "11 : 00"),
+  Event(
+      name: "Linux Fundamentals",
+      club: "DSC NITS",
+      date: "27 Sep",
+      time: "11 : 00"),
   Event(name: "Yoga Class", club: "Yoga Club", date: "27 Sep", time: "11 : 00"),
+];
+
+var clubList = [
+  Club(
+      id: "i1",
+      name: "Illuminits",
+      desc:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non tortor volutpat, molestie tellus vel, luctus elit. Sed hendrerit orci ante, ut venenatis sem cursus non. Etiam ullamcorper condimentum mattis. Etiam commodo dolor ut enim cursus, quis dapibus mi ultrices. Cras sollicitudin sit amet elit id aliquet. Suspendisse scelerisque ultricies vehicula. Curabitur posuere pellentesque maximus. Nulla facilisi.",
+      imageUrl: "assets/illuminits.png"),
+  Club(
+      id: "i2",
+      name: "Machine Learning Club",
+      desc:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non tortor volutpat, molestie tellus vel, luctus elit. Sed hendrerit orci ante, ut venenatis sem cursus non. Etiam ullamcorper condimentum mattis. Etiam commodo dolor ut enim cursus, quis dapibus mi ultrices. Cras sollicitudin sit amet elit id aliquet. Suspendisse scelerisque ultricies vehicula. Curabitur posuere pellentesque maximus. Nulla facilisi.",
+      imageUrl: "assets/ml.png"),
+  Club(
+      id: "i3",
+      name: "DSC NITS",
+      desc:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non tortor volutpat, molestie tellus vel, luctus elit. Sed hendrerit orci ante, ut venenatis sem cursus non. Etiam ullamcorper condimentum mattis. Etiam commodo dolor ut enim cursus, quis dapibus mi ultrices. Cras sollicitudin sit amet elit id aliquet. Suspendisse scelerisque ultricies vehicula. Curabitur posuere pellentesque maximus. Nulla facilisi.",
+      imageUrl: "assets/dsc.png"),
+  Club(
+      id: "i4",
+      name: "NERDS",
+      desc:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non tortor volutpat, molestie tellus vel, luctus elit. Sed hendrerit orci ante, ut venenatis sem cursus non. Etiam ullamcorper condimentum mattis. Etiam commodo dolor ut enim cursus, quis dapibus mi ultrices. Cras sollicitudin sit amet elit id aliquet. Suspendisse scelerisque ultricies vehicula. Curabitur posuere pellentesque maximus. Nulla facilisi.",
+      imageUrl: "assets/nerds.png"),
+  Club(
+      id: "i5",
+      name: "Coding Club",
+      desc:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non tortor volutpat, molestie tellus vel, luctus elit. Sed hendrerit orci ante, ut venenatis sem cursus non. Etiam ullamcorper condimentum mattis. Etiam commodo dolor ut enim cursus, quis dapibus mi ultrices. Cras sollicitudin sit amet elit id aliquet. Suspendisse scelerisque ultricies vehicula. Curabitur posuere pellentesque maximus. Nulla facilisi.",
+      imageUrl: "assets/coding.png"),
 ];
