8e1676d31209c519c25e5657b757243811a0071d
guilefoylegaurav <grvb1515@gmail.com>
2020-11-06 22:44:45 +0530

    Enclosing the banner within a card for better aesthetics. #3

--- lib/widgets/banner.dart
+++ lib/widgets/banner.dart
@@ -7,66 +7,73 @@ class BannerS extends StatelessWidget {
       onTap: () {
         Navigator.of(context).pushNamed("/event");
       },
-      child: Container(
-          width: double.infinity,
-          child: Column(
-            children: [
-              Image(
-                image: AssetImage('assets/hack.jpg'),
-                width: double.infinity,
-                fit: BoxFit.cover,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(25),
+        child: Card(
+          elevation: 5,
+          child: Container(
+              width: double.infinity,
+              child: Column(
                 children: [
+                  Image(
+                    image: AssetImage('assets/go.png'),
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
                   SizedBox(
-                    width: 20,
+                    height: 10,
                   ),
-                  Text(
-                    "Hacktoberfest Workshop ",
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  Text("by "),
-                  Text(
-                    "DSC",
-                    style: TextStyle(color: Colors.blue),
-                  )
-                ],
-              ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(18, 8, 18, 8),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      child: Row(
-                        children: [
-                          Icon(
-                            Icons.timer,
-                            color: Colors.blue,
-                          ),
-                          SizedBox(
-                            width: 8,
-                          ),
-                          Text(
-                            "27 Sep | 21 : 00",
-                            style: TextStyle(color: Colors.blue),
-                          ),
-                        ],
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        width: 20,
                       ),
+                      Text(
+                        "GoLang Workshop ",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      Text("by "),
+                      Text(
+                        "DSC",
+                        style: TextStyle(color: Colors.blue),
+                      )
+                    ],
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(18, 8, 18, 8),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          child: Row(
+                            children: [
+                              Icon(
+                                Icons.timer,
+                                color: Colors.blue,
+                              ),
+                              SizedBox(
+                                width: 8,
+                              ),
+                              Text(
+                                "27 Sep | 21 : 00",
+                                style: TextStyle(color: Colors.blue),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Text(
+                          "ENDS IN 15 M",
+                          style: TextStyle(color: Colors.green),
+                        )
+                      ],
                     ),
-                    Text(
-                      "ENDS IN 15 M",
-                      style: TextStyle(color: Colors.green),
-                    )
-                  ],
-                ),
-              ),
-            ],
-          )),
+                  ),
+                ],
+              )),
+        ),
+      ),
     );
   }
 }
