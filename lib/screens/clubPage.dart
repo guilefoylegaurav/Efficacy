258120442ab7eaf1e7ae3d9eb4789b134780f33a
guilefoylegaurav <grvb1515@gmail.com>
2020-11-06 22:51:13 +0530

    Adding routes to club page, which transfer information about club. Adding tab screens to the tabs. #3

--- lib/screens/clubPage.dart
+++ lib/screens/clubPage.dart
@@ -1,79 +1,101 @@
 import 'package:dsc_club_management_app/config.dart';
+import 'package:dsc_club_management_app/models/club.dart';
+import 'package:dsc_club_management_app/services/data.dart';
+import 'package:dsc_club_management_app/utilities/utilities.dart';
+import 'package:dsc_club_management_app/widgets/description.dart';
 import 'package:dsc_club_management_app/widgets/eventTile.dart';
 import 'package:flutter/material.dart';
-import '../utilities/utilities.dart';
-import '../services/data.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
-class ClubPage extends StatelessWidget {
+class MainCollapsingToolbar extends StatefulWidget {
+  @override
+  _MainCollapsingToolbarState createState() => _MainCollapsingToolbarState();
+}
+
+class _MainCollapsingToolbarState extends State<MainCollapsingToolbar> {
   @override
   Widget build(BuildContext context) {
     final routeArgs =
         ModalRoute.of(context).settings.arguments as Map<String, String>;
     final id = routeArgs['id'];
-    var club = clubList.firstWhere((element) => element.id == id);
-    var events = eventList.where((element) => element.club == club.name);
+    final club = clubList.firstWhere((element) => element.id == id);
+    final event = eventList.where((element) => element.club == club.name);
     return Scaffold(
-      backgroundColor: Color(hexColor(BG)),
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            floating: false,
-            pinned: true,
-            flexibleSpace: FlexibleSpaceBar(
-                centerTitle: true,
-                title: Text(
-                  club.name,
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-                background: Stack(
-                  children: [
-                    Container(
-                      child: Image.network(
-                        "https://lh3.googleusercontent.com/proxy/dvv3PVmRr-U82XIbhzrfJt57vBgAtE7uk1y8wye3dbhWgt-l9lnJjyKDR33uvdjRMi6wPnhqP9N4QpXcLLyfkkLKn65TBuPOwfI9AUGEGK6EW24kvNyO54Aj1xzpgTijSfLy9pr0Ip-5g-NA1P3Nrprz0qzSvT2iYqXV",
+      body: DefaultTabController(
+        length: 2,
+        child: NestedScrollView(
+            headerSliverBuilder:
+                (BuildContext context, bool innerBoxIsScrolled) {
+              return <Widget>[
+                SliverAppBar(
+                  iconTheme: IconThemeData(color: Colors.black),
+                  expandedHeight: 200.0,
+                  floating: false,
+                  pinned: true,
+                  flexibleSpace: FlexibleSpaceBar(
+                      centerTitle: true,
+                      title: Text(club.name,
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.bold)),
+                      background: Image.asset(
+                        club.imageUrl,
                         fit: BoxFit.cover,
-                        width: double.infinity,
-                      ),
+                      )),
+                ),
+                SliverPersistentHeader(
+                  delegate: _SliverAppBarDelegate(
+                    TabBar(
+                      labelColor: Colors.black87,
+                      unselectedLabelColor: Colors.grey,
+                      tabs: [
+                        Tab(text: "Events"),
+                        Tab(text: "Description"),
+                      ],
                     ),
-                    Positioned(
-                      child: Container(
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.only(
-                                topLeft: Radius.circular(30),
-                                topRight: Radius.circular(30)),
-                            color: Colors.white),
-                        height: 600,
-                      ),
-                    )
-                  ],
-                )),
-            expandedHeight: 200,
-          ),
-          SliverList(
-              delegate: SliverChildListDelegate([
-            Container(
-              padding: EdgeInsets.fromLTRB(10, 5, 10, 5),
-              child: SingleChildScrollView(
-                child: Column(children: [
-                  SizedBox(
-                    height: 10,
                   ),
-                  Container(
-                    child: Column(
-                      children: events.map((e) => EventTile(e: e)).toList(),
-                    ),
-                  )
-                ]),
+                  floating: true,
+                ),
+              ];
+            },
+            body: TabBarView(children: [
+              SingleChildScrollView(
+                child: Column(
+                  children: event
+                      .map((e) => EventTile(
+                            e: e,
+                          ))
+                      .toList(),
+                ),
               ),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(30),
-                      topRight: Radius.circular(30)),
-                  color: Colors.white),
-              height: 600,
-            )
-          ]))
-        ],
+              DescriptionSection(club: club),
+            ])),
       ),
     );
+  }
+}
+
+class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
+  _SliverAppBarDelegate(this._tabBar);
+
+  final TabBar _tabBar;
+
+  @override
+  double get minExtent => _tabBar.preferredSize.height;
+  @override
+  double get maxExtent => _tabBar.preferredSize.height;
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Container(
+      child: _tabBar,
+    );
+  }
+
+  @override
+  bool shouldRebuild(_SliverAppBarDelegate oldDelegate) {
+    return false;
   }
 }
