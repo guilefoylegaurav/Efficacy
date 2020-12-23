3151f1c726f5a7a022ebb1483cce5f345a5551d1
guilefoylegaurav <grvb1515@gmail.com>
2020-12-23 11:57:49 +0530

    Added Event Model for event schema in cloud. #7

---
+++ lib/models/eventCloud.dart
@@ -0,0 +1,13 @@
+class EventCloud {
+  String title, picture, clubId, clubName, about;
+  DateTime timings;
+  String id;
+  EventCloud(
+      {this.id,
+      this.title,
+      this.picture,
+      this.clubId,
+      this.clubName,
+      this.about,
+      this.timings});
+}
