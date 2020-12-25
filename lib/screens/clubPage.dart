537accf47fb90fbfa521b9aa9c666e11e879c14a
guilefoylegaurav <grvb1515@gmail.com>
2020-12-25 14:27:16 +0530

    Making club page consume state of the corresponding club, so that the details of that particular club can be viewed in the page. #7

--- lib/screens/clubPage.dart
+++ lib/screens/clubPage.dart
@@ -1,9 +1,13 @@
 import 'package:Efficacy/config.dart';
+import 'package:Efficacy/models/club.dart';
 import 'package:Efficacy/services/data.dart';
+import 'package:Efficacy/services/database.dart';
 import 'package:Efficacy/utilities/utilities.dart';
 import 'package:Efficacy/widgets/description.dart';
+import 'package:Efficacy/widgets/loaders/loader.dart';
 // import 'package:Efficacy/widgets/eventTile.dart';
 import 'package:Efficacy/widgets/sabt.dart';
+import 'package:Efficacy/widgets/tile.dart';
 import 'package:flutter/material.dart';
 
 class ClubPage extends StatefulWidget {
@@ -17,83 +21,105 @@ class _ClubPageState extends State<ClubPage> {
     final routeArgs =
         ModalRoute.of(context).settings.arguments as Map<String, String>;
     final id = routeArgs['id'];
-    final club = clubList.firstWhere((element) => element.id == id);
-    final event = eventList.where((element) => element.club == club.name);
-    return Container(
-      child: Scaffold(
-        body: DefaultTabController(
-          length: 2,
-          child: NestedScrollView(
-              headerSliverBuilder:
-                  (BuildContext context, bool innerBoxIsScrolled) {
-                return <Widget>[
-                  SliverAppBar(
-                    elevation: 0,
-                    iconTheme: IconThemeData(color: Colors.black),
-                    expandedHeight: 200.0,
-                    floating: false,
-                    pinned: true,
-                    flexibleSpace: FlexibleSpaceBar(
-                        background: Image.asset(
-                          club.imageUrl,
-                          fit: BoxFit.cover,
-                        ),
-                        centerTitle: true,
-                        title: SABT(
-                            child: Text(
-                          club.name,
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ))),
-                  ),
-                  SliverPersistentHeader(
-                    delegate: _SliverAppBarDelegate(
-                      TabBar(
-                        indicator: BoxDecoration(
-                            borderRadius: BorderRadius.only(
-                                topLeft: Radius.circular(0),
-                                topRight: Radius.circular(0)),
-                            color: Colors.white),
-                        labelPadding: EdgeInsets.symmetric(
-                            horizontal: 2.0, vertical: 1.0),
-                        labelColor: Color(hexColor(BG)),
-                        unselectedLabelColor: Colors.grey,
-                        tabs: [
-                          Tab(child: Icon(Icons.event)),
-                          Tab(
-                            child: Icon(Icons.info),
+
+    return StreamBuilder<Object>(
+        stream: DatabaseService(id: id).fetchClub,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            Club club = snapshot.data;
+
+            return Container(
+              child: Scaffold(
+                body: DefaultTabController(
+                  length: 2,
+                  child: NestedScrollView(
+                      headerSliverBuilder:
+                          (BuildContext context, bool innerBoxIsScrolled) {
+                        return <Widget>[
+                          SliverAppBar(
+                            elevation: 0,
+                            iconTheme: IconThemeData(color: Colors.black),
+                            expandedHeight: 200.0,
+                            floating: false,
+                            pinned: true,
+                            flexibleSpace: FlexibleSpaceBar(
+                                background: Image.network(
+                                  club.imageUrl,
+                                  fit: BoxFit.cover,
+                                ),
+                                centerTitle: true,
+                                title: SABT(
+                                    child: Text(
+                                  club.name,
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ))),
                           ),
-                        ],
-                      ),
-                    ),
-                    floating: true,
-                  ),
-                ];
-              },
-              body: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(50),
-                ),
-                child: TabBarView(children: [
-                  SingleChildScrollView(
-                    child: Column(
-                        //   children: event
-                        //       .map((e) => EventTile(
-                        //             e: e,
-                        //           ))
-                        //       .toList(),
+                          SliverPersistentHeader(
+                            delegate: _SliverAppBarDelegate(
+                              TabBar(
+                                indicator: BoxDecoration(
+                                    borderRadius: BorderRadius.only(
+                                        topLeft: Radius.circular(0),
+                                        topRight: Radius.circular(0)),
+                                    color: Colors.white),
+                                labelPadding: EdgeInsets.symmetric(
+                                    horizontal: 2.0, vertical: 1.0),
+                                labelColor: Color(hexColor(BG)),
+                                unselectedLabelColor: Colors.grey,
+                                tabs: [
+                                  Tab(child: Icon(Icons.event)),
+                                  Tab(
+                                    child: Icon(Icons.info),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            floating: true,
+                          ),
+                        ];
+                      },
+                      body: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
                         ),
-                  ),
-                  SingleChildScrollView(child: DescriptionSection(club: club)),
-                ]),
-              )),
-        ),
-      ),
-      decoration: BoxDecoration(
-        color: Color(hexColor(BG)),
-      ),
-    );
+                        child: TabBarView(children: [
+                          Text("Under construction"),
+                          // widget(child: EventsbyClub()),
+                          SingleChildScrollView(
+                              child: DescriptionSection(club: club)),
+                        ]),
+                      )),
+                ),
+              ),
+              decoration: BoxDecoration(
+                color: Color(hexColor(BG)),
+              ),
+            );
+          } else {
+            return Loader();
+          }
+        });
   }
 }
+
+// class EventsbyClub extends StatelessWidget {
+//   const EventsbyClub({
+//     Key key,
+//   }) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return SingleChildScrollView(
+//       child: Column(
+//             children: event
+//                 .map((e) => EventTile(
+//                       e: e,
+//                     ))
+//                 .toList(),
+//           ),
+//     );
+//   }
+// }
 
 class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
   _SliverAppBarDelegate(this._tabBar);
