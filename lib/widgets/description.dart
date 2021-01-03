bf77202096ce2ff4aefc9812c5b6ed8b43d78969
guilefoylegaurav <grvb1515@gmail.com>
2021-01-03 21:00:05 +0530

    Adding newline rendering support for the event and club description section.#8

--- lib/widgets/description.dart
+++ lib/widgets/description.dart
@@ -1,6 +1,7 @@
 import 'package:Efficacy/models/club.dart';
 import 'package:Efficacy/services/data.dart';
 import 'package:Efficacy/utilities/utilities.dart';
+import 'package:Efficacy/widgets/line.dart';
 import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
@@ -16,90 +17,100 @@ class DescriptionSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var availableClubs = clubList.where((element) {
-      return club.name != element.name;
-    });
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            club.name,
-            style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
-          ),
-          ListTile(
-            leading: Icon(MdiIcons.web, color: Colors.grey[400]),
-            title: Text("Link"),
-          ),
-          Text(
-            "About Us",
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Text(club.desc),
-          Padding(
-            padding: EdgeInsets.fromLTRB(50, 30, 50, 30),
-            child: Container(
-              height: 1.0,
-              width: double.infinity,
-              color: Color(hexColor(grayBG)),
-            ),
-          ),
-          Text(
-            "Discover More",
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          // Container(
-          //   height: 250,
-          //   child: ListView(
-          //     scrollDirection: Axis.horizontal,
-          //     children: availableClubs.map((e) {
-          //       return Container(
-          //         width: 200,
-          //         child: InkWell(
-          //           onTap: () => {
-          //             Navigator.of(context).pushReplacementNamed("/oneClub",
-          //                 arguments: {"id": e.id})
-          //           },
-          //           child: Card(
-          //             elevation: 5.0,
-          //             margin: EdgeInsets.fromLTRB(10, 0, 10, 10),
-          //             child: Wrap(
-          //               children: [
-          //                 Image.asset(
-          //                   e.imageUrl,
-          //                   width: double.infinity,
-          //                   fit: BoxFit.cover,
-          //                 ),
-          //                 ListTile(
-          //                   title: Text(e.name),
-          //                 )
-          //               ],
-          //             ),
-          //           ),
-          //         ),
-          //       );
-          //     }).toList(),
-          //   ),
-          // ),
-          // SingleChildScrollView(
-          //   scrollDirection: Axis.horizontal,
-          //   child: Row(
-          //     children: clubList
-          //         .map((e) => Card(
-          //               child: Text(e.name),
-          //             ))
-          //         .toList(),
-          //   ),
-          // ),
-        ],
+    return Container(
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      Padding(
+        padding: const EdgeInsets.fromLTRB(15, 25, 15, 25),
+        child: Text(
+          club.name,
+          style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+        ),
       ),
-    );
+      Padding(
+        padding: EdgeInsets.fromLTRB(15, 0, 15, 25),
+        child: Text(
+          "About Us",
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+      ),
+      Padding(
+        padding: EdgeInsets.fromLTRB(15, 0, 15, 25),
+        child: Text(
+          club.desc.replaceAll('/n', '\n'),
+        ),
+      ),
+      Line(L: 50, R: 50, T: 25, B: 25),
+      Padding(
+        padding: const EdgeInsets.fromLTRB(15, 25, 15, 25),
+        child: Text(
+          "Social Links",
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+      ),
+      Padding(
+        padding: EdgeInsets.fromLTRB(10, 0, 15, 25),
+        child: Row(children: [
+          IconButton(
+              icon: Icon(
+                MdiIcons.facebook,
+                color: Colors.blue,
+              ),
+              onPressed: () {}),
+          // IconButton(
+          //     icon: Icon(
+          //       MdiIcons.instagram,
+          //       color: Colors.pink,
+          //     ),
+          //     onPressed: () {}),
+          // IconButton(
+          //     icon: Icon(
+          //       MdiIcons.twitter,
+          //       color: Colors.lightBlue,
+          //     ),
+          //     onPressed: () {}),
+        ]),
+      ),
+      Line(L: 50, R: 50, T: 25, B: 25),
+      Padding(
+        padding: const EdgeInsets.fromLTRB(15, 25, 15, 25),
+        child: Text(
+          "People",
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.fromLTRB(15, 0, 15, 25),
+        child: Text(
+          "This feature is under development. Coming soon",
+        ),
+      ),
+
+      // Padding(
+      //   padding: const EdgeInsets.all(8.0),
+      //   child: Container(
+      //     margin: EdgeInsets.fromLTRB(15, 25, 15, 25),
+      //     height: 200.0,
+      //     child: ListView(
+      //       scrollDirection: Axis.horizontal,
+      //       children: <Widget>[
+      //         Card(
+      //           child: Container(
+      //             width: 160.0,
+      //             child: Column(
+      //               children: [
+      //                 Expanded(
+      //                   child: Row(
+      //                       children: [Expanded(child: Icon(Icons.person))]),
+      //                 ),
+      //                 Text("Admin")
+      //               ],
+      //             ),
+      //           ),
+      //         ),
+      //       ],
+      //     ),
+      //   ),
+      // ),
+    ]));
   }
 }
